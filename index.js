55372ea4e3a1bf7b0da9384cc67ee761ae557789
vivanraaj <vivraaj@gmail.com>
2023-06-29 15:45:39 +0800

    info2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Koa = require("koa");
 const Router = require("@koa/router");
 const { isReady, PrivateKey, Field, Signature } = require("snarkyjs");
 
-const PORT = process.env.PORT || 3000;  sadsadsad
+const PORT = process.env.PORT || 3000;
 
 const app = new Koa();
 const router = new Router();
